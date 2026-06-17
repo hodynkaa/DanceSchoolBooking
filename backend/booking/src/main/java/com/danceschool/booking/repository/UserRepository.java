239290097404c573df3049d4a7d8ed928fc54a13
hodynkaa <katyahour@gmail.com>
2026-06-17 22:45:19 +0200

    go2x

---
+++ backend/booking/src/main/java/com/danceschool/booking/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.danceschool.booking.repository;
+
+import com.danceschool.booking.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
